702303c4df655f2417208545c9f1911a6cf1c5ec
Svetlana Zaiats <svetlana_zaiats@epam.com>
2025-03-14 18:30:49 +0400

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,79 @@
+import '@epam/uui-components/styles.css';
+import '@epam/uui/styles.css';
+import '@epam/assets/theme/theme_loveship.scss';
+import './index.module.scss';
+import logo from './icons/logo.svg';
+//
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserHistory } from 'history';
+import { Route, Router } from 'react-router-dom';
+import {
+    DragGhost,
+    HistoryAdaptedRouter,
+    useUuiServices,
+    UuiContext,
+} from '@epam/uui-core';
+import { MainMenu, MainMenuButton } from '@epam/uui';
+import { ErrorHandler } from '@epam/uui';
+import { Modals, Snackbar } from '@epam/uui-components';
+import { svc } from './services';
+
+import { MainPage } from './pages/MainPage';
+import HomePage from './pages/HomePage';
+import OtherPage from './pages/OtherPage';
+
+const history = createBrowserHistory();
+const router = new HistoryAdaptedRouter(history);
+
+function UuiEnhancedApp() {
+    const { services } = useUuiServices({ router });
+    Object.assign(svc, services);
+    return (
+        <UuiContext.Provider value={services}>
+            <ErrorHandler>
+                <Router history={history}>
+                    <Route>
+                        <MainMenu appLogoUrl={logo}>
+                            <MainMenuButton
+                                caption="Start"
+                                link={{ pathname: '/' }}
+                                priority={1}
+                                estimatedWidth={72}
+                            />
+                            <MainMenuButton
+                                caption="Home"
+                                link={{ pathname: '/home' }}
+                                priority={1}
+                                estimatedWidth={72}
+                            />
+                            <MainMenuButton
+                                caption="Other"
+                                link={{ pathname: '/other' }}
+                                priority={1}
+                                estimatedWidth={72}
+                            />
+                        </MainMenu>
+                    </Route>
+                    <Route path="/" component={MainPage} />
+                    <Route path="/home" component={HomePage} />
+                    <Route path="/other" component={OtherPage} />
+                </Router>
+                <Snackbar />
+                <Modals />
+                <DragGhost />
+            </ErrorHandler>
+        </UuiContext.Provider>
+    );
+}
+
+function initApp() {
+    const root = createRoot(window.document.getElementById('root') as Element);
+    root.render(
+        <StrictMode>
+            <UuiEnhancedApp />
+        </StrictMode>,
+    );
+}
+
+initApp();
